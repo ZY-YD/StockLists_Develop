7be3c7d6d8d7b2321fb2d65380995d5b39db309b
Zhenglong Wu <35463548+ZL-Wu@users.noreply.github.com>
2018-09-26 20:08:13 -0400

    Add the First Version of implementation
    First adding.

---
+++ Assn2_StockList_Students_2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+    public partial class StockList
+    {
+
+        //param   (StockList)listToMerge : second list to be merged 
+        //summary      : merge two different list into a single result list
+        //return       : merged list
+        //return type  : StockList
+        public StockList MergeList(StockList listToMerge)
+        {
+            StockList resultList = new StockList();
+            StockNode currentOne = this.head;
+            StockNode currentTwo = listToMerge.head;
+            while (currentOne != null)
+            {
+                //resultList.AddStock(currentOne.StockHolding);  //A bug in mergeList()
+                while (currentTwo != null)
+                {
+                    resultList.AddStock(currentTwo.StockHolding);
+                    currentTwo = currentTwo.Next;
+                }
+                resultList.AddStock(currentOne.StockHolding);
+                currentOne = currentOne.Next;
+            }
+            return resultList;
+        }
+
+        //param        : NA
+        //summary      : finds the stock with most number of holdings
+        //return       : stock with most shares
+        //return type  : Stock
+        public Stock MostShares()
+        {
+            Stock mostShareStock = null;
+            StockNode current = this.head;
+            StockNode most = this.head;
+            while(current != null)
+            {
+                decimal currentHoldings = current.StockHolding.Holdings;
+                decimal mostHoldings = most.StockHolding.Holdings;
+                if (currentHoldings > mostHoldings)
+                    most = current;
+                current = current.Next;
+            }
+            mostShareStock = most.StockHolding;
+            return mostShareStock;
+        }
+
+        //param        : NA
+        //summary      : finds the number of nodes present in the list
+        //return       : length of list
+        //return type  : int
+        public int Length()
+        {
+            int length = 0;
+            StockNode current = this.head;
+            while(current != null)
+            {
+                current = current.Next;
+                length++;
+            }
+            return length;
+        }
+    }
+}
---
+++ Assn2_StockList_Students_3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_2
+{
+    public partial class StockList
+    {
+        //param        : NA
+        //summary      : Calculate the value of each node by multiplying holdings with price, and returns the total value
+        //return       : total value
+        //return type  : decimal
+        public decimal Value()
+        {
+            decimal value = 0.0m;
+            StockNode current = this.head;
+            while (current != null)
+            {
+                decimal currentHoldings = current.StockHolding.Holdings;
+                decimal currentPrices = current.StockHolding.CurrentPrice;
+                value += currentHoldings * currentPrices;
+                current = current.Next;
+            }
+            return value;
+        }
+
+        //param  (StockList) listToCompare     : StockList which has to comared for similarity index
+        //summary      : finds the similar number of nodes between two lists
+        //return       : similarty index
+        //return type  : int
+        public int Similarity(StockList listToCompare)
+        {
+            int similarityIndex = 0;
+            StockNode currentOne = this.head;
+            StockNode currentTwo = listToCompare.head;
+            while (currentOne != null)
+            {
+                while (currentTwo != null)
+                {
+                    string currentName = currentOne.StockHolding.Name;
+                    string compareName = currentTwo.StockHolding.Name;
+                    if (currentName.CompareTo(compareName) == 0)
+                        similarityIndex++;
+                    currentTwo = currentTwo.Next;
+                }
+                currentTwo = listToCompare.head;
+                currentOne = currentOne.Next;
+            }
+            return similarityIndex;
+        }
+
+        //param        : NA
+        //summary      : Print all the nodes present in the list
+        //return       : NA
+        //return type  : NA
+        public void Print()
+        {
+            StockNode current = this.head;
+            while (current != null)
+            {
+                Console.WriteLine(current.StockHolding.ToString());
+                current = current.Next;
+            }
+        }
+    }
+}
